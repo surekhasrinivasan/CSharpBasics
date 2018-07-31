947281c155146b8440585e9a371f0d76cf6f3a8f
surekhasrinivasan <surekhasrao@gmail.com>
2018-07-31 18:08:15 -0400

    CSharpBasics Initial commit

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -10,6 +10,23 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string Name = "";
+                int Age = 0;
+
+                Console.WriteLine("Enter Name: ");
+                Name = Console.ReadLine();
+                Console.WriteLine("Enter Age: ");
+                Age = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine("Hello " + Name + " " + Age);
+                Console.Read();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Please try ....invalid data");
+            }
         }
     }
 }
