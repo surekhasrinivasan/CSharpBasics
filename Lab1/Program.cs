67118fe6ed8cd377587e1202aa02a161042e8922
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-01 22:45:12 -0400

    Create a class file and organize the code

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -17,29 +17,43 @@ namespace Lab1
             {
                 try
                 {
-                    string Name = "";
-                    int Age = 0;
+                    //string Name = "";
+                    //int Age = 0;
 
+                    //Console.WriteLine("Enter Name: ");
+                    //Name = Console.ReadLine();
+                    //if (Name.Length == 0)
+                    //{
+                    //    Console.WriteLine("Name is required");
+                    //    Console.Read();
+                    //    return;
+                    //}
+
+                    //Console.WriteLine("Enter Age: ");
+                    //Age = Convert.ToInt16(Console.ReadLine());
+                    //if (Age > 100)
+                    //{
+                    //    Console.WriteLine("Invalid Age");
+                    //    Console.Read();
+                    //    return;
+                    //}
+                    //Console.WriteLine("Hello " + Name + " " + Age);
+                    //Console.Read();
+
+                    Person obj = new Person();
                     Console.WriteLine("Enter Name: ");
-                    Name = Console.ReadLine();
-                    if (Name.Length == 0)
-                    {
-                        Console.WriteLine("Name is required");
-                        Console.Read();
-                        return;
-                    }
-
+                    obj.Name = Console.ReadLine();
                     Console.WriteLine("Enter Age: ");
-                    Age = Convert.ToInt16(Console.ReadLine());
-                    if (Age > 100)
-                    {
-                        Console.WriteLine("Invalid Age");
-                        Console.Read();
-                        return;
-                    }
+                    obj.Age = Convert.ToInt16(Console.ReadLine());
 
-                    Console.WriteLine("Hello " + Name + " " + Age);
-                    Console.Read();
+                    if (obj.Valid())
+                    {
+                        Console.WriteLine("Name " + obj.Name + "Age " + obj.Age);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please try ....invalid data");
+                    }
                 }
                 catch (Exception e)
                 {
@@ -47,6 +61,27 @@ namespace Lab1
                 }
                 Console.ReadLine();
             }
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public decimal Salary { get; set; }
+
+        public bool Valid()
+        {
+            if (Name.Length == 0)
+            {
+                return false;
+            }
+            if(Age > 100)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
