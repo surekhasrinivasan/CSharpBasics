b64cb32509c6232760860513ba433a3c6d647986
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-15 08:57:03 -0400

    Different Datatypes

---
+++ UdemyCSharpFundamentals/DataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        //Different data types 
+        int number = 5;
+        Console.WriteLine(number);
+
+        string str = "Hello World!";
+        Console.WriteLine(str);
+
+        char letter = 's';
+        Console.WriteLine(letter);
+
+        bool isEqual = true;
+        Console.WriteLine(isEqual);
+
+        float mathPi = 3.14F;
+        Console.WriteLine(mathPi);
+
+        double mathPinum = 3.14;
+        Console.WriteLine(mathPinum);
+
+        Thread.Sleep(5000);
+    }
+}
+
