d40abe38527fefb075c3a234bcbbbad5a646076f
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-17 05:55:31 -0400

    Difference between Console.Write and Console.WriteLine

---
+++ UdemyCSharpFundamentals/WorkingWithTheConsole/01.WriteAndWriteLine/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        //Difference between Write and WriteLine is Write, writes all in one line whereas WriteLine creates new line
+
+        Console.Write("I am learning ");
+        Console.Write("C# course ");
+        Console.Write("on Udemy\n");
+        Console.WriteLine();
+
+        Console.WriteLine("I am learning ");
+        Console.WriteLine("C# course ");
+        Console.WriteLine("on Udemy\n");
+        Console.WriteLine();
+
+        Thread.Sleep(3000);
+    }
+}
+
