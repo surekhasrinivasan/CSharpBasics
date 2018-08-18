3d2f90ce439e8f99f1a90c64cb94d61ea9b86c04
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-18 11:28:46 -0400

    Logical Operators - AND, OR, XOR, NOT

---
+++ UdemyCSharpFundamentals/OperatorsAndExpressions/LogicalOperators/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+//Logical Operators:
+
+// AND - &&
+// OR - ||
+// XOP - ^
+//NOT - !
+
+class Program
+{
+    static void Main()
+    {
+        bool theTrue = true;
+        bool theFalse = false;
+
+        Console.WriteLine(theTrue && theFalse);
+        Console.WriteLine(theTrue || theFalse);
+        Console.WriteLine(theTrue ^ theFalse);
+        Console.WriteLine(!theTrue);
+
+        Console.WriteLine((theTrue || theFalse) && theFalse);
+        Console.WriteLine((theTrue || theFalse) && (theFalse ^ theTrue));        
+    }
+}
+
