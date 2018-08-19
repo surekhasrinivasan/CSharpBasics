9fd9f1524ed1ef300e6ff85dd6e2b6a9ac50a894
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-19 07:29:30 -0400

    For Loop

---
+++ UdemyCSharpFundamentals/Loops/For/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            if (i == 1)
+            {
+                Console.WriteLine("The loop was executed {0} time!", i);
+            }
+            else
+            {
+                Console.WriteLine("The loop was executed {0} times!", i);
+            }
+        }
+
+        for (int i = 10; i >= 1; i--)
+        {
+            if (i == 1)
+            {
+                Console.WriteLine("The loop was executed {0} time!", i);
+            }
+            else
+            {
+                Console.WriteLine("The loop was executed {0} times!", i);
+            }
+        }
+    }
+}
+
