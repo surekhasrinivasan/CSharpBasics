bad7ea17052388a0997907a3eda019f9fce03a35
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-19 07:11:11 -0400

    While loop

---
+++ UdemyCSharpFundamentals/Loops/WhileDoWhile/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please input the first number:");
+        int firstNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please input the end number:");
+        int endNumber = int.Parse(Console.ReadLine());
+
+        while(firstNumber <= endNumber)
+        {
+            Console.WriteLine(firstNumber);
+            firstNumber++;
+        }
+    }
+}
+
