c0864bfd1abf225bbb2d87c6ad8825ff639ed7e0
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-20 22:13:58 -0400

    Simple Calculator performs addition operation

---
+++ UdemyCSharpFundamentals/SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        //Simple Calculator - Reads two integers from the user and adds them together. Prints the result on console. 
+
+        static void Main()
+        {
+            Console.WriteLine("Please enter first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            int answer = firstNumber + secondNumber;
+
+            Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, answer);
+        }
+    }
+}
