e38f8535737060470317f1daf253543d9c15bd62
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-22 06:27:52 -0400

    Program that calculates hours and minutes from total minutes

---
+++ UdemyCSharpFundamentals/MinutesToHourMinutesCalculation/MinutesToHourMinutesCalculation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MinutesToHourMinutesCalculation
+{
+    class Program
+    {
+        static void Main()
+        {
+            //write a program that asks the user to enter a total amount of time in minutes only. 
+            //Print to the console the same amount of time in hours and minutes.
+
+            Console.WriteLine("Enter total amount of time in minutes only: ");
+            int totalTimeInMinutes = int.Parse(Console.ReadLine());
+
+            int hours = totalTimeInMinutes / 60;
+            int remainingMinutes = totalTimeInMinutes % 60;
+
+            Console.WriteLine("{0} minutes is equal to {1} hours and {2} minutes.", totalTimeInMinutes, hours, remainingMinutes);
+        }
+    }
+}
