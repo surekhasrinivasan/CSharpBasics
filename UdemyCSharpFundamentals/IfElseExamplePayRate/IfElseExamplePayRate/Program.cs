d5e3dd422362e236c76aa4d2337244cbb31dfe37
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-23 06:54:11 -0400

    Example for IfElse - Program that validates hourly pay rate

---
+++ UdemyCSharpFundamentals/IfElseExamplePayRate/IfElseExamplePayRate/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IfElseExamplePayRate
+{
+    class Program
+    {
+        //Write a program that prompts the user for an hourly pay rate. 
+        //If the value entered is less than $7.50 or greater than $49.00, display an error message, 
+        //otherwise display a message indicating that the rate is okay.
+
+        static void Main()
+        {
+            Console.WriteLine("Please enter the hourly pay rate: ");
+            double payRate = double.Parse(Console.ReadLine());
+
+            if(payRate < 7.50 || payRate > 49.00)
+            {
+                Console.WriteLine("Invalid pay rate");
+            }
+            else
+            {
+                Console.WriteLine("The pay rate is OK");
+            }
+        }
+    }
+}
