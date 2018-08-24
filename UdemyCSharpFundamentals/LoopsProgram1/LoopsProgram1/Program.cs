908f3cb033e4f7a34a3e1e318807fb480ef5d0d1
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-24 06:43:57 -0400

    Loops - Program to prompt user until a valid pay rate is entered

---
+++ UdemyCSharpFundamentals/LoopsProgram1/LoopsProgram1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LoopsProgram1
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* Write a console-based application that prompts a user for an
+            *hourly pay rate. While the user enters values less than $5.65
+            *or greater than $49.99, continue to prompt the user. Before
+            *the program ends, display the valid pay rate
+            * 
+            **/
+
+            double payRate;
+
+            do
+            {
+                Console.WriteLine("Please enter valid hourly pay rate");
+                payRate = double.Parse(Console.ReadLine());
+
+            } while (payRate < 5.65 || payRate > 49.99);
+
+            Console.WriteLine("The valid hourly pay rate you entered is {0}", payRate);
+        }
+    }
+}
