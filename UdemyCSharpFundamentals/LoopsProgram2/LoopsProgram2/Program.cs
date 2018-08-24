eda3d0c61adaab8476b1ceba9b3669e8b30697a4
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-24 06:57:19 -0400

    Loops - Program to find the sum of integers

---
+++ UdemyCSharpFundamentals/LoopsProgram2/LoopsProgram2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LoopsProgram2
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Write a console based application that sums the integers from 1 to 50.
+
+            int sum = 0; 
+
+            for(int i = 1;i <= 50; i++)
+            {
+                //Console.WriteLine(i);
+                sum += i;                
+            }
+            Console.WriteLine("The sum of integers from 1 to 50 is {0}",sum);
+        }
+    }
+}
