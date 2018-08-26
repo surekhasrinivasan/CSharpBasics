edc8a11eb3153daebf0fa3c85f00c1fb2f7d25b8
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-26 06:51:24 -0400

    Calculating average grade

--- UdemyCSharpFundamentals/ArraysExample2/ArraysExample2/Program.cs
+++ UdemyCSharpFundamentals/ArraysExample2/ArraysExample2/Program.cs
@@ -14,6 +14,30 @@ namespace ArraysExample2
              *  for the class.
              * */
 
+            Console.WriteLine("Enter the amount of students in your class");
+            int amountOfStudents = int.Parse(Console.ReadLine());
+
+            double averageGrade = 0;
+            double highestGrade = 0;
+            
+            string[] studentNames = new string[amountOfStudents];
+            double[] grades = new double[amountOfStudents];
+
+            for (int i = 0; i < amountOfStudents; i++)
+            {
+                Console.WriteLine("Enter student #{0} name", i + 1);
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter student #{0} grade", i + 1);
+                double grade = double.Parse(Console.ReadLine());
+                grades[i] = grade;
+                averageGrade += grade;                
+            }
+
+            //all the below 3 syntax mean the same
+            //double average = averageGrade / amountOfStudents;
+            //averageGrade = averageGrade / amountOfStudents;
+            averageGrade /= amountOfStudents;
+            Console.WriteLine("The average grade of the class is {0}", averageGrade);
         }
     }
 }
