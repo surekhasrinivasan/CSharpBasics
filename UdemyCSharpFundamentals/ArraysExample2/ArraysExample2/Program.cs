68afc1b073e1832ebe6ed1953cc2b8e696483ef9
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-26 06:48:49 -0400

    Array - Example 2 Program calculating students average grade and highest grade

---
+++ UdemyCSharpFundamentals/ArraysExample2/ArraysExample2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ArraysExample2
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*
+             * 
+             *  Write a program that allows a teacher to enter in an amount of students. For each
+             *  student ask the teacher to enter in their name and final score for the class. When
+             *  the teacher is done entering the data, print the highest grade in the class, and the average grade
+             *  for the class.
+             * */
+
+        }
+    }
+}
