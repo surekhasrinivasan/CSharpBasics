a62c7b40d8012947bcbdb6933faa6236d623b29b
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-28 18:05:11 -0400

    Array methods - Binary Search method

---
+++ UdemyCSharpFundamentals/ArrayMethods/ArrayMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayMethods
+{
+    class Program
+    {
+        //Useful Array Methods
+
+        static void Main()
+        {
+            //Binary search method
+            int[] myArray = { 10, 15, 20, 25, 30, 35, 40, 45 };
+            Console.WriteLine("Enter number to check if it exists");
+            int number = int.Parse(Console.ReadLine());
+
+            int answer = Array.BinarySearch(myArray, number);
+            if(answer < 0)
+            {
+                Console.WriteLine("The number does not exist");
+            }
+            else
+            {
+                Console.WriteLine("The numbers position in the array is {0}", answer);
+            }
+        }
+    }
+}
