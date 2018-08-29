9fb92c4a867b6d508f696906099614510eb04b58
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-29 18:53:15 -0400

    Multidimensional Arrays - Rectangular Array

--- UdemyCSharpFundamentals/MultiArray/MultiArray/Program.cs
+++ UdemyCSharpFundamentals/MultiArray/MultiArray/Program.cs
@@ -8,8 +8,36 @@ namespace MultiArray
 {
     class Program
     {
+        //Multidimension Arrays
+        //Rectangular Array and Jagged Array
+
         static void Main(string[] args)
         {
+            //Rectangular Array
+            int[,] myArray = new int[3, 4];
+            myArray[0, 0] = 5;
+            myArray[0, 1] = 6;
+            myArray[0, 2] = 7;
+            myArray[0, 3] = 8;
+
+            myArray[1, 0] = 5;
+            myArray[1, 1] = 6;
+            myArray[1, 2] = 7;
+            myArray[1, 3] = 8;
+
+            myArray[2, 0] = 5;
+            myArray[2, 1] = 6;
+            myArray[2, 2] = 7;
+            myArray[2, 3] = 8;
+
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    Console.Write(myArray[i,j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
