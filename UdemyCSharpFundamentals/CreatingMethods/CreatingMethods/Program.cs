f0247f635d1a795c0c3bd183fb02bafcadeea1a7
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-30 22:26:13 -0400

    Creating Add two numbers method

--- UdemyCSharpFundamentals/CreatingMethods/CreatingMethods/Program.cs
+++ UdemyCSharpFundamentals/CreatingMethods/CreatingMethods/Program.cs
@@ -11,12 +11,21 @@ namespace CreatingMethods
         static void Main(string[] args)
         {
             DisplayMessage();
+            Add(10, 34);
+            Add(300, 34);
         }
 
         //access modifiers public private, static, return type, methodname(parameters)
         public static void DisplayMessage()
         {
             Console.WriteLine("Hello World!");
+        }
+
+        //Adding two numbers 
+        public static void Add(int num1, int num2)
+        {
+            int answer = num1 + num2;
+            Console.WriteLine(answer);
         }
     }
 }
