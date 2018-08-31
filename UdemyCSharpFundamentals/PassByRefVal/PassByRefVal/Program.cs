9cd93045aa43eeabe85d985216149d1bc612b509
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-31 15:21:10 -0400

    Pass by Value

--- UdemyCSharpFundamentals/PassByRefVal/PassByRefVal/Program.cs
+++ UdemyCSharpFundamentals/PassByRefVal/PassByRefVal/Program.cs
@@ -8,8 +8,20 @@ namespace PassByRefVal
 {
     class Program
     {
+        //Pass by Reference vs. pass by Value
+
         static void Main(string[] args)
         {
+            //Pass by Value
+            int student1Grade = 75;
+            Console.WriteLine("Students grade before extra credit {0}", student1Grade);
+            GiveExtraCredit(student1Grade);
+            Console.WriteLine("Students grade AFTER extra credit {0}", student1Grade);
+        }
+        public static void GiveExtraCredit(int studentsGrade)
+        {
+            studentsGrade += 3;
+            Console.WriteLine("Students grade after extra credit inside function {0}", studentsGrade);
         }
     }
 }
