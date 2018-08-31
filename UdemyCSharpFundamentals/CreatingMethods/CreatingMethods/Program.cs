698ee6e9d41d0deeea4c9e29e9a2a78dc8e0315a
surekhasrinivasan <surekhasrao@gmail.com>
2018-08-30 22:14:07 -0400

    Creating Methods - initial commit

---
+++ UdemyCSharpFundamentals/CreatingMethods/CreatingMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMessage();
+        }
+
+        //access modifiers public private, static, return type, methodname(parameters)
+        public static void DisplayMessage()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
