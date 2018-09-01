2077309009635445170ee1b7bbe9d6f1f3b21a44
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-01 07:25:19 -0400

    Ref Keyword - initial commit

---
+++ UdemyCSharpFundamentals/RefKeyword/RefKeyword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefKeyword
+{
+    class Program
+    {
+        //Ref Keyword
+
+        static void Main(string[] args)
+        {
+            int studentGrade = 75;
+            GiveExtraCredit(studentGrade);
+            Console.WriteLine(studentGrade);
+        }
+
+        public static void GiveExtraCredit(int studentGrade)
+        {
+            studentGrade += 3;
+        }
+}
