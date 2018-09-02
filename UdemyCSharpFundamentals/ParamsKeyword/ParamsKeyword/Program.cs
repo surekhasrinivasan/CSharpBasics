427224f4dbe396f1f38a0c27b405d3a37d11904b
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-02 08:07:30 -0400

    Example to show usage of Params keyword

--- UdemyCSharpFundamentals/ParamsKeyword/ParamsKeyword/Program.cs
+++ UdemyCSharpFundamentals/ParamsKeyword/ParamsKeyword/Program.cs
@@ -15,10 +15,15 @@ namespace ParamsKeyword
             //param keyword stands for parameter array and its deals with passing an array into a function
             int[] myArray = { 5, 6, 7, 8, 9, 10 };
             PrintArray(myArray);
+            //Params keyword allows you to enter any amount of arguments 
+            Console.WriteLine("Hello {0}, {1}, {2}", 5, 6, 7);
+
+            //by using params keyword lets you to print any number of arguments 
+            PrintArray(5, 6, 7, 8, 9, 10, 23, 45, 22, 3, 33, 22);
         }    
         
         //function that prints all the values of an array 
-        public static void PrintArray(int[] array)
+        public static void PrintArray(params int[] array)
         {
             for(int i = 0; i < array.Length; i++)
             {
