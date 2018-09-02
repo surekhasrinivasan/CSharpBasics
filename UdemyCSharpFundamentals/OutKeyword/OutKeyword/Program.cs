fd38de24be5087c2c72deed767b024feead3854f
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-02 07:48:47 -0400

    Example to show usage of Out Keyword

--- UdemyCSharpFundamentals/OutKeyword/OutKeyword/Program.cs
+++ UdemyCSharpFundamentals/OutKeyword/OutKeyword/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace OutKeyword
 {
     class Program
-    { 
+    {
         //Out Keyword 
 
         static void Main(string[] args)
@@ -15,15 +15,30 @@ namespace OutKeyword
             //ref keyword requires initial value whereas while out keyword requires to assign value inside 
             //the function(it is still pass by reference), out keyword is used to send in back many return values 
 
-            int studentGrade;
-            GiveExtraCredit(out studentGrade);
-            Console.WriteLine(studentGrade);
-        }
+            //    int studentGrade;
+            //    GiveExtraCredit(out studentGrade);
+            //    Console.WriteLine(studentGrade);
+            //}
 
-        public static void GiveExtraCredit(out int studenGrade)
+            //public static void GiveExtraCredit(out int studenGrade)
+            //{
+            //    studenGrade = 0;
+            //    studenGrade += 3;
+            //}
+
+            //Adding program with output parameter
+
+            int add;
+            int mult;
+            AddAndMult(22, 10, out add, out mult);
+            Console.WriteLine(add);
+            Console.WriteLine(mult);
+        }
+            
+        public static void AddAndMult(int a,int b, out int added, out int mult)
         {
-            studenGrade = 0;
-            studenGrade += 3;
+            added = a + b;
+            mult = a * b;
         }
     }
 }
