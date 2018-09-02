b7ee325a341db1c2252ab9fd769ed600e7efc963
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-02 07:39:08 -0400

    pass by reference using Out Keyword

---
+++ UdemyCSharpFundamentals/OutKeyword/OutKeyword/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutKeyword
+{
+    class Program
+    { 
+        //Out Keyword 
+
+        static void Main(string[] args)
+        {
+            //ref keyword requires initial value whereas while out keyword requires to assign value inside 
+            //the function(it is still pass by reference), out keyword is used to send in back many return values 
+
+            int studentGrade;
+            GiveExtraCredit(out studentGrade);
+            Console.WriteLine(studentGrade);
+        }
+
+        public static void GiveExtraCredit(out int studenGrade)
+        {
+            studenGrade = 0;
+            studenGrade += 3;
+        }
+    }
+}
