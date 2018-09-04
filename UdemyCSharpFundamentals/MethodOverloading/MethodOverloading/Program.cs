5a46c705df309a1385237ac9baea4128ca2d6511
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-03 22:55:38 -0400

    Method overloading - with string datatype

--- UdemyCSharpFundamentals/MethodOverloading/MethodOverloading/Program.cs
+++ UdemyCSharpFundamentals/MethodOverloading/MethodOverloading/Program.cs
@@ -13,12 +13,20 @@ namespace MethodOverloading
         static void Main(string[] args)
         {
             Add(5, 6);
+            Add("Hello", "World");
         }
 
         //Add method with integer data type parameters 
         public static void Add(int x, int y)
         {
             int answer = x + y;
+            Console.WriteLine(answer);
+        }
+
+        //Add method with string data type parameters
+        public static void Add(string x, string y)
+        {
+            string answer = x + " " + y;
             Console.WriteLine(answer);
         }
     }
