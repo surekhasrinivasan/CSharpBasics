47d270f7561cd538b40dd831c7bc548fd9d494f7
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-04 22:49:17 -0400

    Named parameters

--- UdemyCSharpFundamentals/OptionalParameters/OptionalParameters/Program.cs
+++ UdemyCSharpFundamentals/OptionalParameters/OptionalParameters/Program.cs
@@ -13,13 +13,17 @@ namespace OptionalParameters
         public static void Main(string[] args)
         {
             Add(5);
-            Add(5, 5);
+
+            //Named parameter 
+            Add(5, z : 5);
         }
 
         //To use optional parameter we use some kind of default value and this value is used if we do not supply one.
-        public static void Add(int x, int y = 1)
+
+        //Named arguments allow us to specify what parameter we are talking about. first num goes to first parameter etc. 
+        public static void Add(int x, int y = 1, int z = 2)
         {
-            int answer = x + y;
+            int answer = x + y + z;
             Console.WriteLine(answer);
         }
     }
