0b82ae5ce349b19bc80718dcf167bd6d669dd514
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-05 17:35:44 -0400

    Methods - Exercise-1 initial commit

---
+++ UdemyCSharpFundamentals/MethodsExercise1/MethodsExercise1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise1
+{
+    class Program
+    {
+        /* 
+         *  Create a console-based application whose Main() method
+            asks the user to input an integer and then calls a method
+            named MultiplicationTable(), which displays the
+            results of multiplying the integer by each of the numbers
+            2 through 10
+         * 
+         * */
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
