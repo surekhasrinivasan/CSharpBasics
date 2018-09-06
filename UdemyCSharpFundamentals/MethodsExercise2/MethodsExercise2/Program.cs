5f489c692b17d7a67f7ab3f8d5ede9028e94ad8b
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-06 10:56:49 -0400

    Methods Exercise-2 initial commit

---
+++ UdemyCSharpFundamentals/MethodsExercise2/MethodsExercise2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise2
+{
+    class Program
+    {
+        /* 
+         *  Create a console-based program whose Main() method
+            prompts the user for an integer value and, in turn, passes
+            the value to a method that squares the number and to
+            a method that cubes the number. The Cube() method
+            should call the Square() method. The Main() method
+            displays the results returned from each of the other methods.
+         * 
+         * */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
