d28c5cdf5425fda649a0f49fd05d6814b63fb27c
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-07 15:10:07 -0400

    Methods Exercise-3 - Method named Sum() accepts any number of integer parameters and displays their sum

--- UdemyCSharpFundamentals/MethodsExercise3/MethodsExercise3/Program.cs
+++ UdemyCSharpFundamentals/MethodsExercise3/MethodsExercise3/Program.cs
@@ -18,7 +18,22 @@ namespace MethodsExercise3
          * */
         static void Main(string[] args)
         {
+            Sum(3);
+            Sum(3, 6, 9);
+            Sum(3, 6, 9, 12, 15);
 
+            int[] myArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Sum(myArray);
+        }
+
+        public static void Sum(params int[] myArray)
+        {
+            int sum = 0;
+            for(int i = 0; i < myArray.Length; i++)
+            {
+                sum += myArray[i];
+            }
+            Console.WriteLine("The sum is {0}", sum);
         }
     }
 }
