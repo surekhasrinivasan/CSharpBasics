6eed7274074c081fa4aa3f0d58b3396fe19792a7
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-07 14:15:07 -0400

    Methods Exercise 3 - initial commit

---
+++ UdemyCSharpFundamentals/MethodsExercise3/MethodsExercise3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise3
+{
+    class Program
+    {
+        /*
+         *  Create a method named Sum() that accepts any number of
+            integer parameters and displays their sum. Write a Main()
+            method that demonstrates the Sum() method works correctly
+            when passed one, three, or five integers, or an array of ten
+            integers
+         * 
+         * */
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
