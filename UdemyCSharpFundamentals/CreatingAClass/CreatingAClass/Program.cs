325bb7884418c05b36b26fbf8bf87ddf3b8a308d
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-11 11:17:06 -0400

    Implementation of SayHello function

--- UdemyCSharpFundamentals/CreatingAClass/CreatingAClass/Program.cs
+++ UdemyCSharpFundamentals/CreatingAClass/CreatingAClass/Program.cs
@@ -10,22 +10,27 @@ namespace CreatingAClass
     {
         static void Main(string[] args)
         {
+            //new - means instantiating the class
+
             Student s1 = new Student();
             s1.Name = "Bob";
             s1.Age = 15;
             s1.FinalGrade = 65;
+            s1.SayHello();
 
             Student s2 = new Student();
             s2.Name = "Frank";
             s2.Age = 12;
             s2.FinalGrade = 90;
+            s2.SayHello();
 
             Student s3 = new Student();
             s3.Name = "Ted";
             s3.Age = 15;
             s3.FinalGrade = 95;
+            s3.SayHello();
 
-            Console.WriteLine(s1.Name);
+            //Console.WriteLine(s1.Name);
         }
     }
 }
--- UdemyCSharpFundamentals/CreatingAClass/CreatingAClass/Student.cs
+++ UdemyCSharpFundamentals/CreatingAClass/CreatingAClass/Student.cs
@@ -12,5 +12,9 @@ namespace CreatingAClass
         public int Age;
         public double FinalGrade;
 
+        public void SayHello()
+        {
+            Console.WriteLine("Hello from {0}", Name);
+        }
     }
 }
