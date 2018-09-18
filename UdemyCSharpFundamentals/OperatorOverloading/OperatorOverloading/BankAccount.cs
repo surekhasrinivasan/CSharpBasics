fe8fa8619c6391f2fd3a299036bf79ee1369c825
surekhasrinivasan <surekhasrao@gmail.com>
2018-09-17 23:01:31 -0400

    created operator overloading

--- UdemyCSharpFundamentals/OperatorOverloading/OperatorOverloading/BankAccount.cs
+++ UdemyCSharpFundamentals/OperatorOverloading/OperatorOverloading/BankAccount.cs
@@ -8,5 +8,38 @@ namespace OperatorOverloading
 {
     class BankAccount
     {
+        private double money;
+        private string owner;
+
+        public BankAccount(double money, string owner)
+        {
+            this.money = money;
+            this.owner = owner;            
+        }
+
+        public double Money
+        {
+            get
+            {
+                return money;
+            }
+        }
+
+        public string Owner
+        {
+            get
+            {
+                return owner;
+            }
+        }
+
+        public static BankAccount operator +(BankAccount bank1, BankAccount bank2)
+        {
+            double newAmountOfMoney = bank1.Money + bank2.Money;
+            string newOwners = bank1.Owner + "+" + bank2.owner;
+
+            BankAccount newBankAccount = new BankAccount(newAmountOfMoney, newOwners);
+            return newBankAccount;
+        }
     }
 }
--- UdemyCSharpFundamentals/OperatorOverloading/OperatorOverloading/Program.cs
+++ UdemyCSharpFundamentals/OperatorOverloading/OperatorOverloading/Program.cs
@@ -11,6 +11,13 @@ namespace OperatorOverloading
         //Operator Overloading
         static void Main(string[] args)
         {
+            BankAccount bank1 = new BankAccount(50, "Bob");
+            BankAccount bank2 = new BankAccount(150, "Fred");
+
+            BankAccount bank3 = bank1 + bank2;
+
+            Console.WriteLine(bank3.Money);
+            Console.WriteLine(bank3.Owner);
         }
     }
 }
